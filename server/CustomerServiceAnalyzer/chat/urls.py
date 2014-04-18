1c97e7e9a15529d087b15a820d1d37ee14a7a82e
Collin Yen <collinyen7@gmail.com>
2014-04-18 11:39:12 -0700

    Got basic chat to work TODO: Apply it to actual pages

--- server/CustomerServiceAnalyzer/chat/urls.py
+++ server/CustomerServiceAnalyzer/chat/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from chat import views
 
 urlpatterns = patterns('',
-        url(r'^chatroom/$', views.chat_room, name='chatroom')  
+        url(r'^employeechatroom/$', views.employee_chat_room, name='employeechatroom'),
+        url(r'^customerchatroom/$', views.customer_chat_room, name='customerchatroom')
         )
--- server/CustomerServiceAnalyzer/chat/views.py
+++ server/CustomerServiceAnalyzer/chat/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
+import constants
+
 # Create your views here.
-def chat_room(request):
-    return render(request, 'chat/chat_room.html')
+def render_chat_room(request, info):
+	return render(request, 'chat/chat_room.html', {'info':info})
+
+def employee_chat_room(request):
+	info = {}
+	info['id'] = 1
+	info['name'] = "Collin Yen"
+	info['entity_type'] = constants.ENTITY_TYPE_EMPLOYEE
+
+	return render_chat_room(request, info)
+    
+
+def customer_chat_room(request):
+	info = {}
+	info['id'] = 2
+	info['name'] = "Misa Campo"
+	info['entity_type'] = constants.ENTITY_TYPE_CUSTOMER
+
+	return render_chat_room(request, info)
